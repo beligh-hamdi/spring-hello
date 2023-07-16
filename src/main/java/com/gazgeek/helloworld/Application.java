59a6e88a275b38565359a01a1bf302041c6a04b6
Beligh HAMDI <beligh.hamdi@gmail.com>
2023-07-15 22:25:05 -0400

    update spring boot

--- src/main/java/com/gazgeek/helloworld/Application.java
+++ src/main/java/com/gazgeek/helloworld/Application.java
@@ -1,17 +1,13 @@
 package com.gazgeek.helloworld;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 
 }
